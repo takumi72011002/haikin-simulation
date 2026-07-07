eba6678f7c31a050f800f2d5e56d1c81ccfd380e
takumi72011002 <takumi72011002@gmail.com>
2026-07-07 09:48:55 +0900

    simulation.pyを更新

--- simulation.py
+++ simulation.py
@@ -13,3 +13,10 @@ name = st.text_input("名前")
 
 if name:
     st.success(f"こんにちは、{name}さん！")
+
+st.markdown("""
+<svg width="60" height="60">
+    <rect x="10" y="10" width="40" height="40"
+          fill="black" />
+</svg>
+""", unsafe_allow_html=True)
